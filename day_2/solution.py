5f2ac5f8dfbadb04c87d5dcb4ac46a6c8d5b1cf1
Memoey <git@memoey.icu>
2024-10-25 10:39:20 +0200

    perf: solution without division
    calculcate sub products of elements to the left and right of current element

--- day_2/solution.py
+++ day_2/solution.py
@@ -8,12 +8,19 @@ def create_new_array_from_sums(original_arr: list) -> list:
     array is the product of all the numbers in 
     the original array except the one at i.
     """
-    product_of_all = 1
+    new_arr = [1 for _ in original_arr]
+    sub_product = 1
 
-    for element in original_arr:
-        product_of_all *= element
+    # calculate product of all elements to the left
+    for idx, element in enumerate(original_arr):
+        new_arr[idx] *= sub_product
+        sub_product *= element
 
-    new_arr = []
-    for element in original_arr:
-        new_arr.append(product_of_all/element)
+    sub_product = 1
+    # calculcate product of all elements to the left
+    # multiplied by product of all elements to the right
+    for idx in range(len(original_arr) - 1, -1, -1):
+        new_arr[idx] *= sub_product
+        sub_product *= original_arr[idx]
+
     return new_arr
